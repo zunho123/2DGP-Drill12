8318fda08e0b1cefc279938e1527baef13ee298c
kim junho <zunho8331@tukorea.ac.kr>
2025-11-25 11:32:40 +0900

    1. 배회 기본 root

--- Drill #12/zombie.py
+++ Drill #12/zombie.py
@@ -158,14 +158,10 @@ class Zombie:
         a3 = Action('Set random location', self.set_random_location)
         root = wander = Sequence('Wander', a3, a2)
 
-        c1 = Condition('소년이 근처에 있는가?', self.if_boy_nearby, 7)
-        a4 = Action('소년한테 접근', self.move_to_boy)
-        root = chase_boy = Sequence('소년이 가까이 있으면 추적', c1, a4)
+       # c1 = Condition('소년이 근처에 있는가?', self.if_boy_nearby, 7)
+       # a4 = Action('소년한테 접근', self.move_to_boy)
+       # root = chase_boy = Sequence('소년이 가까이 있으면 추적', c1, a4)
 
-        root = chase_or_wander = Selector('소년이 가까이 있으면 추적 아니면 방황', chase_boy, wander)
+       # root = chase_or_wander = Selector('소년이 가까이 있으면 추적 아니면 방황', chase_boy, wander)
 
-        a5 = Action('순찰 위치 가져오기', self.get_patrol_location)
-        root = patrol = Sequence('순찰', a5, a2)
-
-        root = patrol_or_chase = Selector('순찰하다가 소년이 근처에 있으면 추적', chase_boy, patrol)
         self.bt = BehaviorTree(root)
